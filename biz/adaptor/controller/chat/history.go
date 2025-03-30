ebaebd4a6b384cd53944b92daa96eb49e2d1a382
univero <1449610641@qq.com>
2025-03-30 12:50:25 +0800

    fix: 修复音频，添加历史查询

---
+++ biz/adaptor/controller/chat/history.go
@@ -0,0 +1,26 @@
+package chat
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/xh-polaris/psych-digital/biz/adaptor"
+	"github.com/xh-polaris/psych-digital/biz/adaptor/cmd"
+	"github.com/xh-polaris/psych-digital/provider"
+)
+
+// ListHistory .
+// @router /chat/history/list [POST]
+func ListHistory(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req cmd.ListHistoryReq
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		c.String(consts.StatusBadRequest, err.Error())
+		return
+	}
+
+	p := provider.Get()
+	resp, err := p.HistoryService.ListHistory(ctx, &req)
+	adaptor.PostProcess(ctx, c, &req, resp, err)
+}
